50d47508dee92a0bf05c677ca334d123cf64ebcf
Sethu-madhav <sethu4784@gmail.com>
2025-01-01 19:45:25 -0500

    format code

--- vector-add.cu
+++ vector-add.cu
@@ -1,24 +1,28 @@
-#include<stdio.h>
-#include<iostream>
-#include<cuda_runtime.h>
+#include <stdio.h>
+#include <iostream>
+#include <cuda_runtime.h>
 
 // CPU function
-void addVectors(const float *a, const float *b, float *c, int n){
-    for(int i = 0; i < n; i++){
+void addVectors(const float *a, const float *b, float *c, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         c[i] = a[i] + b[i];
     }
     return;
 }
 
-int main(){
+int main()
+{
     float *a, *b, *c;
     int n = 10;
-    size_t size = n*sizeof(float);
-    a = (float*)malloc(size);
-    b = (float*)malloc(size);
-    c = (float*)malloc(size);
+    size_t size = n * sizeof(float);
+    a = (float *)malloc(size);
+    b = (float *)malloc(size);
+    c = (float *)malloc(size);
 
-    for(int i=0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         a[i] = i;
         b[i] = i;
     }
@@ -26,8 +30,8 @@ int main(){
     addVectors(a, b, c, n);
 
     std::cout << std::endl;
-    for(int i=0; i<n; i++)
-        std::cout<< c[i] << " ";
+    for (int i = 0; i < n; i++)
+        std::cout << c[i] << " ";
 
     free(a);
     free(b);
